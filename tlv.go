a9aba3877658cb736aab6971f4a88368a7ca0b5e
root <root@localhost.localdomain>
2023-08-18 11:22:04 +0800

    项目提交

---
+++ tlv.go
@@ -0,0 +1,21 @@
+package tlv
+
+//tlv操作接口
+
+type Container interface {
+	GetByte(key uint8) (b byte, exists bool)
+	GetBytes(key uint8) (b []byte, exists bool)
+
+	SetByte(key uint8, value byte)
+	SetBytes(key uint8, value []byte)
+
+	Remove(key uint8)
+
+	Bytes() []byte
+
+	// Size returns the length of the byte slice or buffer
+	// Size() int
+
+	// NoEntries returns the number of nodes in the container
+	NoEntries() int
+}
